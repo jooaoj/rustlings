331c997b15166d623962f5a43e1b3b248660e16d
Jooa Jaakkola <jaakkola.jooa@gmail.coma>
2024-11-19 23:21:56 +0200

    quiz 1

--- README.md
+++ README.md
@@ -2,9 +2,37 @@
 
 Practical training for the Rust programming language!
 
+## Progress
+
+|Quizzes|Completed|
+|---|---|
+|1|X|
+|2||
+|3||
+
 |Exercise|Completed|
 |---|---|
-| 0 | X |
-| 1 | X |
-| 2 | X |
-| 3 | X |
+|0|X|
+|1|X|
+|2|X|
+|3|X|
+|4||
+|5||
+|6||
+|7||
+|8||
+|9||
+|10||
+|11||
+|12||
+|13||
+|14||
+|15||
+|16||
+|17||
+|18||
+|19||
+|20||
+|21||
+|22||
+|23||
--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,10 +10,18 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(apples: i32) -> i32 {
+    if apples > 40 {
+        apples
+    } else {
+        apples*2
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
+    let count = 41;
+    println!("{count} apples costs {} rustbucks", calculate_price_of_apples(count));
 }
 
 // Don't change the tests!
