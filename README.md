bd040cc86ab85658903f0b4d866e1a3a40c4824d
Jooa Jaakkola <jaakkola.jooa@gmail.coma>
2024-11-19 22:01:38 +0200

    exercise 1

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Practical training for the Rust programming language!
 
 | Exercise | Completed |
 | -------- | --------- |
-|     1    |           |
+|     0    |     X     |
+|     1    |     X     |
--- exercises/01_variables/variables3.rs
+++ exercises/01_variables/variables3.rs
@@ -1,6 +1,14 @@
 fn main() {
     // TODO: Change the line below to fix the compiler error.
-    let x: i32;
+    let x: i32 = 0;
 
-    println!("Number {x}");
+    if x == 42 {
+        println!("Number {x}, according to guide 👍");
+    }
+    else if x == 69 {
+        println!("Number {x}, nice!");
+    }
+    else {
+        println!("Number {x}");
+    }
 }
